aadf669a6337e930311e3873cfd10a0c614fb31f
ndrsonya <sofia.altusar-anderson@suomenterveysravinto.fi>
2024-12-18 14:02:29 +0200

    remove unused

--- src/controllers/deviceController.ts
+++ src/controllers/deviceController.ts
@@ -1,9 +1,9 @@
-import { Request, Response, NextFunction, RequestHandler } from 'express';
+import { Request, Response } from 'express';
 import { fetchDevicesByUserId } from '../repositories/deviceRepository';
 import { Device } from '../types/deviceTypes';
 import { handleResponse, handleError } from '../helpers/responseHandler';
 
-export const getDevicesByUserId = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+export const getDevicesByUserId = async (req: Request, res: Response): Promise<void> => {
     const { user_id } = req.params;
 
     try {
